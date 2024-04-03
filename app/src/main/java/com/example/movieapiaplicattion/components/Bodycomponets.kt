8c33e4e5b1a11360dbfa2ec03af9e6b7be953cea
yan_ortiz <yanortiz@wposs.com>
2024-04-03 12:00:57 -0500

    implementacion del cardview para las imagenes en bodycomponents

--- app/src/main/java/com/example/movieapiaplicattion/components/Bodycomponets.kt
+++ app/src/main/java/com/example/movieapiaplicattion/components/Bodycomponets.kt
@@ -1,7 +1,15 @@
 package com.example.movieapiaplicattion.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -9,9 +17,18 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Scale
+import com.example.movieapiaplicattion.R
+import com.example.movieapiaplicattion.model.PokemonList
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -32,5 +49,42 @@ fun MainTopBar(title: String, showBackButton: Boolean = false, onClickAnction: (
                 }
             }
         }
+    )
+}
+
+@Composable
+fun CardPokemon(pokemonList: PokemonList, pokeimage: String, onClickAnction: () -> Unit) {
+    Card(
+        shape = RoundedCornerShape(5.dp),
+        modifier = Modifier
+            .padding(10.dp)
+            .shadow(40.dp)
+            .clickable { onClickAnction }
+
+    ) {
+        Column {
+            MainImage(image = pokeimage)
+        }
+    }
+}
+
+@Composable
+fun MainImage(image: String) {
+
+    val painter = rememberAsyncImagePainter(
+        ImageRequest.Builder(LocalContext.current).data(data = image)
+            .apply(block = fun ImageRequest.Builder.() {
+                error(R.drawable.images)
+                scale(Scale.FILL)
+            }).scale(Scale.FILL)
+            .build()
+    )
+    Image(
+        painter = painter,
+        contentDescription = "Image from URL",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(250.dp)
     )
 }
--- app/src/main/java/com/example/movieapiaplicattion/views/HomeView.kt
+++ app/src/main/java/com/example/movieapiaplicattion/views/HomeView.kt
@@ -1,9 +1,7 @@
 package com.example.movieapiaplicattion.views
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -13,12 +11,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
-import coil.request.ImageRequest
-import coil.size.Scale
-import com.example.movieapiaplicattion.R
+import androidx.compose.ui.graphics.Color
+import com.example.movieapiaplicattion.components.CardPokemon
 import com.example.movieapiaplicattion.components.MainTopBar
 import com.example.movieapiaplicattion.viewmodel.PokemonViewModel
 
@@ -40,38 +34,22 @@ fun ContentHomeView(viewModel: PokemonViewModel, paddingValues: PaddingValues) {
     val pokemon by viewModel.pokemon.collectAsState()
     val pokemon1 by viewModel.pokemon1.collectAsState()
 
-    LazyColumn(modifier = Modifier.padding(paddingValues)) {
+    LazyColumn(
+        modifier = Modifier
+            .padding(paddingValues)
+            .background(Color.Black)
+    ) {
         items(pokemon) { item ->
-            Text(text = item.name)
-        }
-
-        if (pokemon1.isNotEmpty()) {
-            val urls = pokemon1.split(",").filter { it.isNotEmpty() }
-            urls.forEach { url ->
-                item {
-                    LoadImageFromUrl(url)
-                    Spacer(modifier = Modifier.height(16.dp))
+            if (pokemon1.isNotEmpty()) {
+                val urls = pokemon1.split(",").filter { it.isNotEmpty() }
+                urls.forEach { url ->
+                    CardPokemon(pokemonList = item, pokeimage = url) {
+                    }
                 }
             }
+            Text(text = item.name)
         }
-
     }
 }
 
-@Composable
-fun LoadImageFromUrl(pokemon1: String) {
 
-    val painter = rememberAsyncImagePainter(
-        ImageRequest.Builder(LocalContext.current).data(data = pokemon1)
-            .apply(block = fun ImageRequest.Builder.() {
-                error(R.drawable.images)
-                scale(Scale.FILL)
-            }).scale(Scale.FILL)
-            .build()
-    )
-    Image(
-        painter = painter,
-        contentDescription = "Image from URL",
-        modifier = Modifier.height(100.dp)
-    )
-}
