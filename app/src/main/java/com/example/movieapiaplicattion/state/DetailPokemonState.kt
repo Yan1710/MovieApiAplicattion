35b532a8e1854be38c6fb9fc9cd73cc862c472f4
Yan Marcos Ortiz Martinez <ortizyan45@gmail.com>
2024-04-07 20:58:32 -0500

    IMPLEMENTACION - ORDEN DEL SCAFFOLD -Se implemetan el scaffold al detailview.

--- app/src/main/java/com/example/movieapiaplicattion/state/DetailPokemonState.kt
+++ app/src/main/java/com/example/movieapiaplicattion/state/DetailPokemonState.kt
@@ -3,6 +3,7 @@ package com.example.movieapiaplicattion.state
 data class DetailPokemonState(
     val name: String = "",
     val order: Int = 0,
-    val weight: Int = 0
+    val weight: Int = 0,
+    val front_default:String
 )
 
--- app/src/main/java/com/example/movieapiaplicattion/views/DetailView.kt
+++ app/src/main/java/com/example/movieapiaplicattion/views/DetailView.kt
@@ -1,18 +1,31 @@
 package com.example.movieapiaplicattion.views
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.navigation.NavController
+import com.example.movieapiaplicattion.components.MainTopBar
 import com.example.movieapiaplicattion.viewmodel.PokemonViewModel
 
 @Composable
 fun DetailView(viewModel: PokemonViewModel, navController: NavController, name: String) {
+    Scaffold(topBar = {
+        MainTopBar(title = "LISTA DE POKEMONES",true) {
+            navController.popBackStack()
+        }
+    }) {
+         ContentDeailView( viewModel,  it,  navController,name)
+    }
+}
+@Composable
+fun ContentDeailView(viewModel: PokemonViewModel, paddingValues: PaddingValues, navController: NavController, name: String){
     val pokemon1 by viewModel.pokemon1.collectAsState()
     LaunchedEffect(Unit) {
         viewModel.fetchDetailPokemon(name)
     }
-    Text(text = viewModel.state.order.toString())
+
 }
